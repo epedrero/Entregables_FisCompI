0562c8ae64316b722ec212d281b7da2cbd3469e7
epedrero <e.pedrero01@ufromail.cl>
2023-05-25 19:51:44 +0000

    Tarea 2 primera version

--- Tarea2/Problema1/2/Trapecio.c
+++ Tarea2/Problema1/2/Trapecio.c
@@ -1,13 +1,13 @@
 #include "header2.h"
 
 // Implementación del método del trapecio para la integración
-double metodoTrapecio(double a, double b, int n, double (*func)(double, double, double, double, double), double m, double k, double b_val, double mu)  {
-    double h = (b - a) / n; // Ancho
-    double suma = ((*func)(a, m, k, b_val, mu) + (*func)(b, m, k, b_val, mu)) / 2.0; // Suma inicial
+double metodoTrapecio(double x0, double xn, int n, double (*func)(double, double, double, double, double), double m, double k, double b, double mu)  {
+    double h = (xn - x0) / n; // Ancho
+    double suma = ((*func)(x0, m, k, b, mu) + (*func)(xn, m, k, b, mu)) / 2.0; // Suma inicial
 
     for (int i = 1; i < n; i++) {
-        double x = a + i * h;
-        suma += (*func)(x, m, k, b_val, mu);
+        double x = x0 + i * h;
+        suma += (*func)(x, m, k, b, mu);
     }
 
     return h * suma;
--- Tarea2/Problema1/3b/main3b.c
+++ Tarea2/Problema1/3b/main3b.c
@@ -3,20 +3,22 @@
 #include "header2.h"
 
 int main() {
+    // Parámetros de la función aceleración
     double m = 0.8; // Masa
     double b = 0.4; // Longitud natural
     double mu = 0.3; // Coeficiente de fricción
     double k = 80.0; // Constante de rigidez
 
-    double a = b; // Límite inferior de integración
+    // Parámetros del método del trapecio
     double x_0 = b; // Condición inicial de posición
     double v_0 = 0.0; // Condición inicial de velocidad
+    double x_n = 0.0; // Posición a encontrar
     double n = 1000; // Numero de divisiones
     double y_0 = v_0*v_0/2; // Velocidad inicial transformada
 
     // Calcular la integral de la aceleración utilizando el método del trapecio
-    double integral = y_0 + metodoTrapecio(a, x_0, n, &aceleracion, m, k, b, mu);
-    double rapidez = sqrt(2*fabs(integral));
+    double y = y_0 + metodoTrapecio(x_0, x_n, n, &aceleracion, m, k, b, mu);
+    double rapidez = sqrt(2*fabs(y));
     printf("La rapidez en x=0 es: %lf\n", rapidez);
 
     return 0;
